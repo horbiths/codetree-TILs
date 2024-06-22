864d98baeb4f588a0607b95eb44a338922ade8ed
ORBI <113783690+horbiths@users.noreply.github.com>
2024-06-23 00:50:04 +0900

    Title: 369 게임, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240623/369 게임/369-game.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int ten = i % 10;
+            int one = i - ten;
+            if (i % 3 == 0) {
+                System.out.print("0 ");
+            } else if (ten == 9 || one == 9) {
+                System.out.print("0 ");
+            } else if (ten == 6 || one == 6) {
+                System.out.print("0 ");
+            } else if (ten == 3 || one == 3) {
+                System.out.print("0 ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
