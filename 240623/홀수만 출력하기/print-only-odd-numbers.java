9057bcd3cc35c1cfc31c43b6c9183d918af470bb
ORBI <113783690+horbiths@users.noreply.github.com>
2024-06-23 01:19:13 +0900

    Title: 홀수만 출력하기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 240623/홀수만 출력하기/print-only-odd-numbers.java
+++ 240623/홀수만 출력하기/print-only-odd-numbers.java
@@ -7,11 +7,11 @@ public class Main {
         int N = sc.nextInt();
         int[] inputN = new int[N];
 
-        for (int i = 0; i < N-1; i++) {
+        for (int i = 0; i < N; i++) {
             inputN[i] = sc.nextInt();
         }
 
-        for (int j = 0; j < N-1; j++) {
+        for (int j = 0; j < N; j++) {
             if(inputN[j] % 3 == 0 && inputN[j] % 2 == 1) {
                 System.out.println(inputN[j]);
             }
