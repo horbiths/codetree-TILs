658482f129a81a844525ad03c55a25114062ebe2
ORBI <113783690+horbiths@users.noreply.github.com>
2024-06-23 00:13:57 +0900

    Title: a 부터 b 까지, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240623/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int num = a;
+
+        System.out.print(num + " ");
+        while(num < b) {
+            if (num % 2 == 0) {
+                num += 3;
+                System.out.print(num + " ");
+            } else {
+                num *= 2;
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
